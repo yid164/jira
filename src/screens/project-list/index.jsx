faf080d7d265322d3c6be7322ef7a8e203289c37
Ken Dong <yid164@mail.usask.ca>
2022-01-03 13:05:22 -0600

    added list

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,38 @@
+import {useEffect, useState} from "react";
+import {SearchPanel} from "./search-panel";
+import {List} from "./list";
+import * as qs from "qs"
+import {cleanObject} from "./utils";
+
+const apiURL = process.env.REACT_APP_API_URL
+
+export const ProjectListScreen = () => {
+
+    const [param, setParam] = useState({
+        name: '',
+        personId: ''
+    })
+    const [list, setList] = useState([])
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        fetch(`${apiURL}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+            if (response.ok) {
+                setList(await response.json())
+            }
+        })
+    }, [param])
+
+    useEffect(() => {
+        fetch(`${apiURL}/users`).then(async response => {
+            if (response.ok) {
+                setUsers(await response.json())
+            }
+        })
+    }, [])
+
+    return <div>
+        <SearchPanel users={users} param={param} setParam={setParam}/>
+        <List users={users} list={list}/>
+    </div>
+}
